5047d53112954222cc2fbfdc03d4acd59fd3d064
BaoBau <derekeanton@gmail.com>
2025-06-14 01:12:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const carousel = document.getElementById('recommendations_img_con');
-
 const items = carousel.children;
+
 const firstClone = items[0].cloneNode(true);
 const lastClone = items[items.length - 1].cloneNode(true);
 
@@ -17,15 +17,14 @@ carousel.addEventListener('scroll', () => {
   const children = carousel.children;
   const firstReal = children[1];
   const lastReal = children[children.length - 2];
-  
+
   if (carousel.scrollLeft <= firstReal.offsetLeft - carousel.offsetLeft - 1) {
-    carousel.scrollLeft = lastReal.offsetLeft - carousel.offsetLeft + 1;
+      carousel.scrollLeft = lastReal.offsetLeft - carousel.offsetLeft;
   }
-  
-  if (
+    if (
     carousel.scrollLeft >=
-    lastReal.offsetLeft - carousel.offsetLeft + lastReal.offsetWidth - 1
+    lastReal.offsetLeft - carousel.offsetLeft + lastReal.offsetWidth
   ) {
-    carousel.scrollLeft = firstReal.offsetLeft - carousel.offsetLeft + 1;
+      carousel.scrollLeft = firstReal.offsetLeft - carousel.offsetLeft;
   }
 });
